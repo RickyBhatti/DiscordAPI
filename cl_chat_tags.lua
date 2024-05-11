81e46de2137350eedd0c5d820c340f2850a0b047
RickyBGamez <rickyb5@live.ca>
2024-05-10 20:44:48 -0600

    refactor: clean up spacing.

--- cl_chat_tags.lua
+++ cl_chat_tags.lua
@@ -1,5 +1,5 @@
 TriggerServerEvent("DiscordAPI:UpdateChatPermissions")
 
 TriggerEvent("chat:addSuggestion", "/chattag", "Select the tag that appears beside your name.", {
-    { name = "ID", help = "Chat tag ID." }
+    {name = "ID", help = "Chat tag ID."}
 })
