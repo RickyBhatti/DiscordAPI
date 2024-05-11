07ebbdd62e6b9b16d84ff7199b96ab2b7d387939
RickyBGamez <rickyb5@live.ca>
2024-05-10 21:22:40 -0600

    refactor: localize and cleanup the discord api.

--- sv_discord_api.lua
+++ sv_discord_api.lua
@@ -1,9 +1,22 @@
 local token = "Bot " .. Config.DiscordToken
 
+local pairs = pairs
+local tostring = tostring
+local decode = json.decode
+
+local _PerformHttpRequest = PerformHttpRequest
+local _Wait = Citizen.Wait
+
+local _GetIdentifiersTable = GetIdentifiersTable
+local _Log = Log
+
+local GuildID = Config.GuildID
+local DiscordRoles = Config.DiscordRoles
+
 local function DiscordRequest(endpoint, method, jsondata)
     local data = nil
 
-    PerformHttpRequest("https://discord.com/api/" .. endpoint, function(codeResponse, dataResponse, headersResponse)
+    _PerformHttpRequest("https://discord.com/api/" .. endpoint, function(codeResponse, dataResponse, headersResponse)
         data = {
             code = codeResponse,
             data = dataResponse,
@@ -16,61 +29,52 @@ local function DiscordRequest(endpoint, method, jsondata)
     })
 
     while data == nil do
-        Citizen.Wait(0)
+        _Wait(0)
     end
 
     return data
 end
 
 function getRoles(user)
-    local identifiers = GetIdentifiersTable(user)
+    local identifiers = _GetIdentifiersTable(user)
 
-    if identifiers.discord then
-        local endpoint = ("guilds/%s/members/%s"):format(Config.GuildID, identifiers.discord)
-        local result = DiscordRequest(endpoint, "GET", {})
+    if not identifiers.discord then return {} end
 
-        if result.code == 200 then
-            local data = json.decode(result.data)
-            return data.roles
-        else
-            return {}
-        end
-    else
-        return {}
-    end
+    local endpoint = ("guilds/%s/members/%s"):format(GuildID, identifiers.discord)
+    local result = DiscordRequest(endpoint, "GET", {})
+    if result.code ~= 200 then return {} end
+
+    local data = decode(result.data)
+    return data.roles
 end
 
 function isRolePresent(user, role)
-    local identifiers = GetIdentifiersTable(user)
+    local identifiers = _GetIdentifiersTable(user)
 
-    if identifiers.discord then
-        local endpoint = ("guilds/%s/members/%s"):format(Config.GuildID, identifiers.discord)
-        local result = DiscordRequest(endpoint, "GET", {})
+    if not identifiers.discord then return false end
 
-        if result.code == 200 then
-            local data = json.decode(result.data)
-            local toFind = Config.DiscordRoles[tostring(role)]
+    local endpoint = ("guilds/%s/members/%s"):format(GuildID, identifiers.discord)
+    local result = DiscordRequest(endpoint, "GET", {})
+    if result.code ~= 200 then return false end
 
-            for k, v in pairs(data.roles) do
-                if toFind == v then
-                    return true
-                end
-            end
-        else
-            return false
+    local data = decode(result.data)
+    local toFind = DiscordRoles[tostring(role)]
+
+    for k, v in pairs(data.roles) do
+        if toFind == v then
+            return true
         end
-    else
-        return false
     end
 end
 
 Citizen.CreateThread(function()
-    local guild = DiscordRequest("guilds/" .. Config.GuildID, "GET", {})
+    local guild = DiscordRequest("guilds/" .. GuildID, "GET", {})
 
-    if guild.code == 200 then 
-        local data = json.decode(guild.data)
-        Log("Permission guild was set to: " .. data.name .. " (ID: " .. data.id .. ")")
-    else
-        Log("^1An error has occured with your guild information. (".. (guild.data or guild.code) .. ")")
+    if guild.code ~= 200 then
+        _Log("^1An error has occured with your guild information. (".. (guild.data or guild.code) .. ")")
+        return
     end
+
+    local data = decode(guild.data)
+    _Log("Permission guild was set to: " .. data.name .. " (ID: " .. data.id .. ")")
 end)
