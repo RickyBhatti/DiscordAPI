071324590603252249f6d9833c1eebda886f90d4
RickyBGamez <rickyb5@live.ca>
2024-05-22 20:06:17 -0600

    Update version.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game "gta5"
 name "DiscordAPI"
 description "An all one in solution for Discord API, that controls chat tags, permissions."
 author "ricky"
-version "v2.0-dev"
+version "v2.0.0"
 
 lua54 "yes"
 
