9787ba5967d356d749841613a2b0e464bb193c16
RickyBGamez <rickyb5@live.ca>
2024-06-11 01:42:33 -0600

    Some basic file layout and notes.

---
+++ cl_vehicle_restriction.lua
@@ -0,0 +1,5 @@
+Citizen.CreateThread(function()
+    while true do
+        Wait(0) -- Change this to be a value from the config file, maybe? Might actually do a thread creation once the net event replies.
+    end
+end)
--- sv_vehicle_restriction.lua
+++ sv_vehicle_restriction.lua
@@ -1,1 +1,17 @@
 if not Config.VehicleRestrictionsEnabled then return end
+
+-- TODO: Localizations go here.
+-- TODO: Deal with the data tables, ensure they're O(1) lookup time, ideally.
+
+AddEventHandler("playerConnecting", function()
+    local source = source
+    local identifiers = GetIdentifiersTable(source)
+    local license, discord = identifiers.license, identifiers.discord
+
+    if not discord then return end
+
+    local roles = getRoles(source)
+    if roles == nil then return end
+
+    -- TODO: Logic goes here.
+end)
