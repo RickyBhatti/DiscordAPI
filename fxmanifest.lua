809c375a1ede26818606c347dfd163d96b3c5095
RickyBGamez <rickyb5@live.ca>
2024-07-11 17:35:04 -0600

    chore: bump up version.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game "gta5"
 name "DiscordAPI"
 description "An all one in solution for Discord API, that controls chat tags, permissions."
 author "ricky"
-version "v2.0.0"
+version "v2.0.1"
 
 lua54 "yes"
 
