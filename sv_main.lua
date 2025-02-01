07d33283c4cb571820b6c52429c8c9801a283dd4
RickyBGamez <rickyb5@live.ca>
2025-01-31 18:58:11 -0700

    Version check.

--- sv_main.lua
+++ sv_main.lua
@@ -1,5 +1,6 @@
 Citizen.CreateThread(function()
     local name = GetCurrentResourceName()
+    local version = tostring(GetResourceMetadata(name, "version", 0))
     local principal = "resource." .. name
 
     if name ~= "DiscordAPI" then 
@@ -21,4 +22,17 @@ Citizen.CreateThread(function()
     if not IsPrincipalAceAllowed(principal, "command.remove_ace") then 
         Log("^1WARNING: DiscordAPI is missing the 'command.remove_ace' permission. The resource may not function as intended. Please grant this permission in the server.cfg.")
     end
+
+    PerformHttpRequest("https://raw.githubusercontent.com/RickyBhatti/DiscordAPI/refs/heads/main/version.txt", function(code, text, _)
+        if code ~= 200 then
+            Log("^1WARNING: DiscordAPI was unable to check for updates. Please ensure you have an active internet connection.")
+            return
+        end
+            
+        if version ~= tostring(text) then
+            Log("^1WARNING: Your version of DiscordAPI doesn't match the latest version.")
+            Log("^1WARNING: Your version: ^7" .. version)
+            Log("^1WARNING: Latest version: ^7" .. text)
+        end
+    end)
 end)
